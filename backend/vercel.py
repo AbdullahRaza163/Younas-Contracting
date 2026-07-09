aa5c57c8333a45aa84dc092e71fda310bfae8f63
AbdullahRaza163 <razaabdullah163@gmail.com>
2026-07-09 14:23:19 +0500

    first commit

--- backend/vercel.py
+++ backend/vercel.py
@@ -1,14 +1,7 @@
-{
-  "builds": [
-    {
-      "src": "vercel.py",
-      "use": "@vercel/python"
-    }
-  ],
-  "routes": [
-    {
-      "src": "/(.*)",
-      "dest": "vercel.py"
-    }
-  ]
-}
+from app import app
+
+# Vercel expects this variable named 'app'
+application = app
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
