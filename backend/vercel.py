9c0df5b0ecb096358b073620058360b58ab240b1
AbdullahRaza163 <razaabdullah163@gmail.com>
2026-07-09 14:40:04 +0500

    first commit

--- backend/vercel.py
+++ backend/vercel.py
@@ -1,7 +1,4 @@
 from app import app
 
-# Vercel expects this variable named 'app'
-application = app
-
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+# Vercel looks for a variable named "app"
+app = app
