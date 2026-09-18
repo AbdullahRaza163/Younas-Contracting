4eeaa169cd607a5ee11146c6b49796dc53e65814
AbdullahRaza163 <razaabdullah163@gmail.com>
2026-09-18 16:08:49 +0500

    Screen Updated For Proper calculations

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,6 +1,6 @@
 // config/constants.js
 export const CONFIG = {
-  API_BASE: 'http://127.0.0.1:5000/api',
+  API_BASE: 'https://younas-contracting-backend.onrender.com/api',
   CURRENCY: 'BD',
   COMPANY_NAME: 'Haji Younas Contracting',
   VERSION: '2.0.0'
